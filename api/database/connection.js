451883021f192033e03b3ea649976af23cb105f5
surajpatwal22 <patwal0suraj@gmail.com>
2024-05-27 20:59:59 +0530

    dbconnect

---
+++ api/database/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+
+
+const connectDB = (uri) =>{
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        }).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err));
+}
+
+module.exports = connectDB;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require('express');
 
 const app = express();
-
+const connectDB = require("./database/connection")
+ 
 const port = process.env.port || 4000 ;
 
 const product_routes = require('./routes/productRoute')
@@ -10,10 +12,11 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.use('/api', product_routes )
+app.use('/api/products', product_routes )
 
 const start = async () =>{
     try {
+        await connectDB(process.env.MONGODB_URI);
         app.listen(port,()=>{
             console.log(`Server is running on port ${port}`);
         })
